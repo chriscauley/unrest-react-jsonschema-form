2a8995eca59ed22abdc4ffe19b113e19c593b264
chriscauley <chris@lablackey.com>
2020-04-28 01:03:13 +0000

    introducing handleError to better [puts on sunglasses] handle the errors

--- dist/post.js
+++ dist/post.js
@@ -3,35 +3,54 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports["default"] = exports.handleError = void 0;
 
 var _config = _interopRequireDefault(require("./config"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var handleError = function handleError(error) {
+  // Analyzes many possible exeptions and return { error: "Something the user can read" }
+  if (typeof error === "string") {
+    return {
+      error: error
+    };
+  }
+
+  if (error.name === "TypeError" && error.message === "Failed to fetch") {
+    return {
+      error: "Unable to connect to server. Please check your internet connection and try again."
+    };
+  }
+
+  return {
+    error: error.message
+  };
+};
+
+exports.handleError = handleError;
+
+var resolve = function resolve(response) {
+  var contentType = response.headers.get('content-type');
+
+  if (!contentType || !contentType.includes('application/json')) {
+    // all responses from the server should be an ajax
+    // this is triggered when the server has hit a 500
+    // server can also send back { error } to display a more specific error
+    return {
+      error: _config["default"].unknown_error
+    };
+  }
+
+  return response.json()["catch"](handleError);
+};
 
 var _default = function _default(url, data) {
   return fetch(url, {
     body: JSON.stringify(data),
     method: 'POST',
     headers: _config["default"].getHeaders()
-  }).then(function (response) {
-    var contentType = response.headers.get('content-type');
-
-    if (!contentType || !contentType.includes('application/json')) {
-      // all responses from the server should be an ajax
-      // this is triggered when the server has hit a 500
-      // server can also send back { error } to display a more specific error
-      return {
-        error: _config["default"].unknown_error
-      };
-    }
-
-    return response.json()["catch"](function (error) {
-      return {
-        error: error
-      };
-    });
-  });
+  }).then(resolve, handleError);
 };
 
 exports["default"] = _default;
